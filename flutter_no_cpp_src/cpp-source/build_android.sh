820609f3e2ff2f0adc0b8c02caa45917bdb553d4
cjl_macbook <cjl_spy@163.com>
2019-11-01 21:03:22 +0800

    Update cmakelists and build_android so shell.

--- flutter_no_cpp_src/cpp-source/build_android.sh
+++ flutter_no_cpp_src/cpp-source/build_android.sh
@@ -1,7 +1,7 @@
-cmake \
--DANDROID_ABI=armeabi-v7a \
--DANDROID_PLATFORM=android-15 \
--DCMAKE_BUILD_TYPE=release \
--DANDROID_NDK=/Users/cai/Library/Android/sdk/ndk-bundle \
--DCMAKE_TOOLCHAIN_FILE=/Users/cai/Library/Android/sdk/ndk-bundle/build/cmake/android.toolchain.cmake \
--DANDROID_TOOLCHAIN=clang .
+rm -rf ./build
+a="armeabi-v7a arm64-v8a x86 x86_64"
+for abi in $a;
+do
+export ABI=$abi
+sh cmd/android.sh
+done
