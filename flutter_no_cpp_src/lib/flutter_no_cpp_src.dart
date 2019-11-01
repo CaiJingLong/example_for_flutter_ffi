6217a6d6925ed5e5110f674b0f9ca170973c9cab
cjl_macbook <cjl_spy@163.com>
2019-11-01 21:59:48 +0800

    Update android code to call native method.

--- flutter_no_cpp_src/lib/flutter_no_cpp_src.dart
+++ flutter_no_cpp_src/lib/flutter_no_cpp_src.dart
@@ -1,6 +1,16 @@
 import 'dart:async';
 
 import 'package:flutter/services.dart';
+import 'dart:ffi'; // For FFI
+import 'dart:io'; // For Platform.isX
+
+final DynamicLibrary nativeAddLib = Platform.isAndroid
+    ? DynamicLibrary.open("libsome.so")
+    : DynamicLibrary.open("native_add.framework/native_add");
+
+final int Function(int x, int y) nativeAdd = nativeAddLib
+    .lookup<NativeFunction<Int32 Function(Int32, Int32)>>("native_add")
+    .asFunction();
 
 class FlutterNoCppSrc {
   static const MethodChannel _channel =
@@ -9,5 +19,9 @@ class FlutterNoCppSrc {
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
+  }
+
+  static int addWithCpp(int x, int y) {
+    return nativeAdd(x, y);
   }
 }
