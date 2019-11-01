beee0b92c442bef5d6a0fd39cdd05cc7db9c7567
Caijinglong <cjl_spy@163.com>
2019-11-01 14:24:13 +0800

    Update readme.

--- README.md
+++ README.md
@@ -1,14 +1,10 @@
 # native_add
 
-A new flutter plugin project.
+The example can be run on android/ios.
 
-## Getting Started
+```bash
+cd example
+flutter run
+```
 
-This project is a starting point for a Flutter
-[plug-in package](https://flutter.dev/developing-packages/),
-a specialized package that includes platform-specific implementation code for
-Android and/or iOS.
-
-For help getting started with Flutter, view our 
-[online documentation](https://flutter.dev/docs), which offers tutorials, 
-samples, guidance on mobile development, and a full API reference.
+The `_counter` add action come from native_add of cpp.
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -54,7 +54,7 @@ class _MyHomePageState extends State<MyHomePage> {
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
-      _counter++;
+      _counter = NativeAdd.add(_counter, 1);
     });
   }
 
@@ -98,9 +98,6 @@ class _MyHomePageState extends State<MyHomePage> {
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.display1,
-            ),
-            Text(
-              "${NativeAdd.add(1, 2)}",
             ),
           ],
         ),
